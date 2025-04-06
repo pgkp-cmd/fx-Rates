475d4103ea8b2cc93f96a33ba8b819e914af5940
pgkp-cmd <gichuhipeter32@gmail.com>
2025-04-06 23:07:34 +0300

    Update CurrencySelect.jsx

---
+++ src/components/CurrencySelect.jsx
@@ -0,0 +1,44 @@
+// Currency Array for different currencies
+const currencyCodes = [
+    "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
+    "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL",
+    "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY",
+    "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP",
+    "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS",
+    "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
+    "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD",
+    "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT",
+    "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD",
+    "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
+    "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK",
+    "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR",
+    "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SLL", "SOS", "SRD",
+    "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY",
+    "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES",
+    "VND", "VUV", "WST", "XAF", "XCD", "XOF", "XPF", "YER", "ZAR", "ZMW",
+    "ZWL"
+];
+
+// This component renders a currency select dropdown with a flag icon based on the country code
+const CurrencySelect = ({props, handleCurrencyChange}) => {
+const countryCodes = props.substring(0, 2).toUpperCase();
+
+
+return (
+<div>
+    <img className="rounded-full" src={`https://flagsapi.com/${countryCodes}/flat/64.png`} alt="flags-icon" />
+    <select
+    onChange={handleCurrencyChange}
+    value={props}
+    className="w-full p-2  border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300 mt-1" required>
+      {currencyCodes.map((code => 
+        <option key={code} value={code}>
+          {code}
+        </option>
+      ))}
+    </select>
+    </div>
+)
+}
+
+export default CurrencySelect
