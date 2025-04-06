15a6b6a28f986f5e3e9645b1974ef3f70dbd4f15
pgkp-cmd <gichuhipeter32@gmail.com>
2025-04-06 23:08:53 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function Forms() {
+const Home = () => {
   return (
-    <div>
-        
-    </div>
+    <div className="text-center bg-gray-800 text-white py-8 mt-10">
+
+    <h1 className="text-3xl font-bold text-white mb-2">Currency Converter</h1>
+    <p className="">Get real-time currency conversions with ease. Simply enter the amount,<br />
+    select your currencies, and see the converted value instantly.</p>
+      </div>
   )
 }
 
-export default Forms
+export default Home;
