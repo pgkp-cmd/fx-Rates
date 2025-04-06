23e07522907351224b48f711a2bed4290ef3a34c
pgkp-cmd <gichuhipeter32@gmail.com>
2025-04-06 22:40:03 +0300

    Implemrntation of some key features

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Footer from './components/Footer'
-import Header from './components/Header'
-import Navigation from './components/Navigation'
+import Navigation from './components/Navbar'
 
 function App() {
   return (
     <div>
       <Navigation/>
-      <Header/>
-       
+      <div className="max-w-xl mx-auto my-10 p-10 bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-semibold text-center">Currency Converter</h1>
+      <p>Convert your currency easily and quickly!</p>
+
+      <ConvertArea />
+    </div>
+      
+      <CustomerExperience />
+
       <Footer/>
     </div>
   )
